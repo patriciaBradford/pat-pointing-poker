9d4fa21ef321c92b5f506d4d9dcaa31a523aa530
patriciaBradford <patricia.m.bradford@gmail.com>
2019-02-05 18:39:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
-var config = {
-  apiKey: "AIzaSyAnZzUUZSK7nZPjbkMvmVOwpmx5L0CIP70",
-  authDomain: "pat-scrum-poker.firebaseapp.com",
-  databaseURL: "https://pat-scrum-poker.firebaseio.com",
-  projectId: "pat-scrum-poker",
-  storageBucket: "pat-scrum-poker.appspot.com",
-  messagingSenderId: "296236030876"
-};
-firebase.initializeApp(config);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
